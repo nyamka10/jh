63fd11514dffee9082c4a5de2858a2b5fdae4c00
NYAMKA <kostya111000@icloud.com>
2021-12-26 19:53:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+settings = {
+    'CLIENT_ID':'OtN5MRAP5-dvPEmWC4eAuA',
+    'SECRET_CODE':'PIsJcwnzJB17xjNKuSaeYz_EkxEmVQ',
+    'DISCORD_TOKEN':'OTI0NjczMTM0NjYwMDM0NTcw.Ych-9g.iFC1J80qamNtsfC-A7kOehWonMw'
+}
